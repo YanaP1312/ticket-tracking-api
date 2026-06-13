f1b44aec921e362e810fc5aef69d7f3ad791be89
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-06-13 17:38:32 +0200

    feat: add global exception handling with NotFoundException and BadRequestException

--- src/main/java/net/hackyourfuture/tickettrackingsystem/exceptions/GlobalExceptionHandler.java
+++ src/main/java/net/hackyourfuture/tickettrackingsystem/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package net.hackyourfuture.tickettrackingsystem.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(NotFoundException ex){
+        return Map.of("error", ex.getMessage());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleBadRequest(BadRequestException ex){
+        return Map.of("error", ex.getMessage());
+    }
 }
