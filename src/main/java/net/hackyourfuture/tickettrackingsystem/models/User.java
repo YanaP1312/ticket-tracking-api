fb5bcdf55b64e1e3d9c661fbebb7877d5c937cea
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-06-13 16:43:28 +0200

    feat: add User model with userId, userName and userEmail fields

--- src/main/java/net/hackyourfuture/tickettrackingsystem/models/User.java
+++ src/main/java/net/hackyourfuture/tickettrackingsystem/models/User.java
@@ -1,4 +1,16 @@
 package net.hackyourfuture.tickettrackingsystem.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private int userId;
+    private String userName;
+    private String userEmail;
 }
