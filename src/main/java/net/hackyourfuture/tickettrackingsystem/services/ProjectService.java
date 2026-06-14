2c5523e3b3be11ac7ef93069769e1995141f4c03
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-06-14 17:58:30 +0200

    add project service

--- src/main/java/net/hackyourfuture/tickettrackingsystem/services/ProjectService.java
+++ src/main/java/net/hackyourfuture/tickettrackingsystem/services/ProjectService.java
@@ -1,4 +1,30 @@
 package net.hackyourfuture.tickettrackingsystem.services;
 
+import net.hackyourfuture.tickettrackingsystem.dto.response.projects.GetProjectResponse;
+import net.hackyourfuture.tickettrackingsystem.exceptions.NotFoundException;
+import net.hackyourfuture.tickettrackingsystem.repositories.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProjectService {
+
+    private ProjectRepository repository;
+
+    public ProjectService(ProjectRepository repository){
+        this.repository = repository;
+    }
+
+    public List<GetProjectResponse> getAllProjects(){
+        return repository.getAllProjects();
+    }
+
+    public boolean isProjectExist(int projectId){
+        if(!repository.projectExists(projectId)){
+            throw new NotFoundException("Project with id " + projectId + " not found");
+        }
+
+        return true;
+    }
 }
