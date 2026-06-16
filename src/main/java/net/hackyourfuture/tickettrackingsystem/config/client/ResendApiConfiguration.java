e845cf62950fd38a54da43827a958bd5cb276d43
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-06-16 07:32:11 +0200

    feat: add env, dependencies and configuration for resend

--- src/main/java/net/hackyourfuture/tickettrackingsystem/config/client/ResendApiConfiguration.java
+++ src/main/java/net/hackyourfuture/tickettrackingsystem/config/client/ResendApiConfiguration.java
@@ -1,4 +1,19 @@
 package net.hackyourfuture.tickettrackingsystem.config.client;
 
+import com.resend.Resend;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class ResendApiConfiguration {
+
+    @Value("${resend.api.key}")
+    public String apiKey;
+
+    @Bean
+    public Resend resend(){
+        return new Resend(apiKey);
+    }
 }
